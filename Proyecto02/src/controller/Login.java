6c48ea2a6bacd70ac90648d7a5b92e04cfb52f8d
redhuo <jimmytsafg@gmail.com>
2020-07-30 18:56:19 -0600

    controller

--- Proyecto02/src/controller/Login.java
+++ Proyecto02/src/controller/Login.java
@@ -1,5 +1,127 @@
 package controller;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
-public class Login {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+
+
+@WebServlet("/adminArticulo")
+public class Login  extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+	
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Hola Servlet..");
+		String action = request.getParameter("action");
+		System.out.println(action);
+		try {
+			switch (action) {
+			case "index":
+				index(request, response);
+				break;
+			case "nuevo":
+				nuevo(request, response);
+				break;
+			case "register":
+				System.out.println("entro");
+				registrar(request, response);
+				break;
+			case "mostrar":
+				mostrar(request, response);
+				break;
+			case "showedit":
+				showEditar(request, response);
+				break;	
+			case "editar":
+				editar(request, response);
+				break;
+			case "eliminar":
+				eliminar(request, response);
+				break;
+			default:
+				break;
+			}			
+		} catch (SQLException e) {
+			e.getStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Hola Servlet..");
+		doGet(request, response);
+	}
+	
+	private void index (HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		//mostrar(request, response);
+		RequestDispatcher dispatcher= request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void registrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		//Articulo articulo = new Articulo(0, request.getParameter("codigo"), request.getParameter("nombre"), request.getParameter("descripcion"),request.getParameter("cantidad"),request.getParameter("precio"));
+		//articuloDAO.insertar(articulo);
+		System.out.println("aqui");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void nuevo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/register.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	private void mostrar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException , ServletException{
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/mostrar.jsp");
+		List<Articulo> listaArticulos= articuloDAO.listarArticulos();
+		request.setAttribute("lista", listaArticulos);
+		dispatcher.forward(request, response);
+	}	
+	
+	private void showEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		Articulo articulo = articuloDAO.obtenerPorId(Integer.parseInt(request.getParameter("id")));
+		request.setAttribute("articulo", articulo);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/editar.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void editar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		Articulo articulo = new Articulo(Integer.parseInt(request.getParameter("id")), request.getParameter("codigo"), request.getParameter("nombre"), request.getParameter("descripcion"),request.getParameter("existencia"), request.getParameter("precio"));
+		articuloDAO.actualizar(articulo);
+		index(request, response);
+	}
+	
+	private void eliminar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		Articulo articulo = articuloDAO.obtenerPorId(Integer.parseInt(request.getParameter("id")));
+		articuloDAO.eliminar(articulo);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+		
+	}
 }
