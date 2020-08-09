b836be914de7ba85ab0c14165052a073a6815253
redhuo <jimmytsafg@gmail.com>
2020-08-09 17:11:00 -0600

    ne forms

--- Proyecto02/src/Model/Catalogue.java
+++ Proyecto02/src/Model/Catalogue.java
@@ -3,14 +3,22 @@ package Model;
 public class Catalogue {
 	String id;
 	String name;
-	public Catalogue(String id,String name) {
+	String treat;
+	public Catalogue(String id,String name,String treat) {
 		this.id = id;
-		this.name=id;
+		this.name=name;
+		this.treat = treat;
 	}
 	public String getId() {
 		return id;
 	}
 	public String getName() {
 		return name;
+	}
+	public String getTreat() {
+		return treat;
+	}
+	public void setTreat(String treat) {
+		this.treat = treat;
 	}
 }
--- Proyecto02/src/dao/Cataloguedao.java
+++ Proyecto02/src/dao/Cataloguedao.java
@@ -1,5 +1,143 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Catalogue;
+import Utils.Conexion;
+
+
+
+
 public class Cataloguedao {
+	private Conexion con;
+	private Connection connection;
+
+	public Cataloguedao() throws SQLException {
+	
+		con = new Conexion();
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void insert(Catalogue add) throws SQLException {
+		String sql = "select addcatalogue (?,?,?)";
+		
+		con.conectar();
+		connection = con.getJdbcConnection();
+		  DatabaseMetaData dm = (DatabaseMetaData) connection.getMetaData();
+		System.out.println("Driver name: " + dm.getDriverName()+ "     l     oco");
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString (1,add.getId());
+		statement.setString(2,add.getName());
+		statement.setString(3,add.getTreat());
+
+	
+		 System.out.println("Driver name: " + dm.getDriverName()+ "  washa");
+
+		statement.executeUpdate();
+		
+		 System.out.println("Driver name: " + dm.getDriverName()+ "  washa23");
+		statement.close();
+		con.desconectar();
+	
+		
+	
+	}
+/*	public Categoria obtenerPorId(String id) throws SQLException {
+		Categoria nuevo = null;
+
+		String sql = "SELECT * FROM Categoria WHERE CodigoCategoria= ? ";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, id);
+
+		ResultSet res = statement.executeQuery();
+		if (res.next()) {
+			nuevo = new Categoria(res.getString("CodigoCategoria"), res.getString("NombreCategoria"), res.getString("Descripcion"),
+					res.getString("Estado"));
+		}
+		res.close();
+		con.desconectar();
+
+		return nuevo;
+	}
+	public List<Categoria> listarCategorias() throws SQLException {
+
+		List<Categoria> listaCategorias = new ArrayList<Categoria>();
+		String sql = "SELECT * FROM Categoria";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		Statement statement = connection.createStatement();
+		ResultSet resulSet = statement.executeQuery(sql);
+
+		while (resulSet.next()) {
+			String id = resulSet.getString("CodigoCategoria");
+			String nombre= resulSet.getString("NombreCategoria");
+			String descripcion = resulSet.getString("Descripcion");
+			String estado= resulSet.getString("Estado");
+
+			Categoria nuevo= new Categoria(id, nombre, descripcion,estado);
+			listaCategorias.add(nuevo);
+		}
+		con.desconectar();
+		return listaCategorias;
+	}
+	
+	public boolean eliminar(Categoria nuevo) throws SQLException {
+		boolean rowEliminar = false;
+		String sql = "DELETE FROM Categoria WHERE CodigoCategoria=?";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, nuevo.getCodigoCategoria());
+
+		rowEliminar = statement.executeUpdate() > 0;
+		statement.close();
+		con.desconectar();
+
+		return rowEliminar;
+	}
+	
+	public boolean actualizar(Categoria nuevo) throws SQLException {
+		boolean rowActualizar = false;
+		String sql = "UPDATE Categoria SET CodigoCategoria=?,NombreCategoria=?,Descripcion=?,Estado=? WHERE CodigoCategoria=?";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString (1,nuevo.getCodigoCategoria());
+		statement.setString(2,nuevo.getNombreCategoria());
+		statement.setString(3,nuevo.getDescripcion());
+		statement.setString(4,nuevo.getEstado());
+		statement.setString(5, nuevo.getCodigoCategoria());
+		System.out.println(nuevo.getCodigoCategoria()+ " holaaaaaaaaaaaaaaaa");
+
+		rowActualizar = statement.executeUpdate() > 0;
+		System.out.println(nuevo.getDescripcion()+ " holaaaaaaaaaaaaaaaa "+ rowActualizar ) ;
+		statement.close();
+		con.desconectar();
+		return rowActualizar;
+	}*/
+	
+	public static void main (String args[]) {
+		Catalogue nuevo = new Catalogue("345","Fiebre","Paracetamol");
+		
+		try {
+			new Cataloguedao().insert(nuevo);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
