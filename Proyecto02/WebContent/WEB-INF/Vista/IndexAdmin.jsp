aa16f59e006e8759643f58594d6cadb93fdd2477
redhuo <jimmytsafg@gmail.com>
2020-08-10 19:28:23 -0600

    NEW FORMS

--- Proyecto02/WebContent/WEB-INF/Vista/IndexAdmin.jsp
+++ Proyecto02/WebContent/WEB-INF/Vista/IndexAdmin.jsp
@@ -17,10 +17,6 @@
 </head>
 <title>HospiTec</title>
 </head>
-<body>
-<link
-	href="https://unpkg.com/bootstrap@3.3.7/dist/css/bootstrap.min.css"
-	rel="stylesheet" />
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -37,15 +33,12 @@
           <li><a href="#">Page 1-3</a></li>
         </ul>
       </li>
-      <li><a href="#">Diagnostics</a></li>
-      <li><a href="#">Treatments</a></li>
-       <li><a href="#">Hospitalizations</a></li>
+      <li><a href="#">Centers</a></li>
+      <li><a href="#">Catalogue</a></li>
         <li><a href="#">Sign out</a></li>
     </ul>
   </div>
 </nav>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-</body>
-</html>
+<link
+	href="https://unpkg.com/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+	rel="stylesheet" />>
--- Proyecto02/src/dao/Wokerdao.java
+++ Proyecto02/src/dao/Wokerdao.java
@@ -1,5 +1,149 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import Model.Catalogue;
+
+import Model.User;
+import Model.Worker;
+import Utils.Conexion;
+
+
+
+
 public class Wokerdao {
+	private Conexion con;
+	private Connection connection;
+
+	public Wokerdao() throws SQLException {
 	
+		con = new Conexion();
+	
+	}
+	public void insert(Worker add,User add2,String center) throws SQLException {
+		String sql = "select addpatient (?,?,?,?,?,?,?,?,?,?,?)";
+		
+		con.conectar();
+		connection = con.getJdbcConnection();
+		  DatabaseMetaData dm = (DatabaseMetaData) connection.getMetaData();
+		System.out.println("Driver name: " + dm.getDriverName()+ "     l     oco");
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString (1,add.getId());
+		statement.setString(2,add.getName());
+		statement.setString(3,add.getSurname1());
+		statement.setString(4,add.getSurname2());
+		statement.setString(5,add.getType());
+		statement.setString(6,add.getEntry());
+		statement.setString(7,add2.getUsername());
+		statement.setString(8,add2.getPassword());
+		statement.setString(9,center);
+	
+		 System.out.println("Driver name: " + dm.getDriverName()+ "  washa");
+
+		statement.executeUpdate();
+		
+		 System.out.println("Driver name: " + dm.getDriverName()+ "  washa23");
+		statement.close();
+		con.desconectar();
+	
+		
+	
+	}
+/*	public Categoria obtenerPorId(String id) throws SQLException {
+		Categoria nuevo = null;
+
+		String sql = "SELECT * FROM Categoria WHERE CodigoCategoria= ? ";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, id);
+
+		ResultSet res = statement.executeQuery();
+		if (res.next()) {
+			nuevo = new Categoria(res.getString("CodigoCategoria"), res.getString("NombreCategoria"), res.getString("Descripcion"),
+					res.getString("Estado"));
+		}
+		res.close();
+		con.desconectar();
+
+		return nuevo;
+	}
+	public List<Categoria> listarCategorias() throws SQLException {
+
+		List<Categoria> listaCategorias = new ArrayList<Categoria>();
+		String sql = "SELECT * FROM Categoria";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		Statement statement = connection.createStatement();
+		ResultSet resulSet = statement.executeQuery(sql);
+
+		while (resulSet.next()) {
+			String id = resulSet.getString("CodigoCategoria");
+			String nombre= resulSet.getString("NombreCategoria");
+			String descripcion = resulSet.getString("Descripcion");
+			String estado= resulSet.getString("Estado");
+
+			Categoria nuevo= new Categoria(id, nombre, descripcion,estado);
+			listaCategorias.add(nuevo);
+		}
+		con.desconectar();
+		return listaCategorias;
+	}
+	
+	public boolean eliminar(Categoria nuevo) throws SQLException {
+		boolean rowEliminar = false;
+		String sql = "DELETE FROM Categoria WHERE CodigoCategoria=?";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, nuevo.getCodigoCategoria());
+
+		rowEliminar = statement.executeUpdate() > 0;
+		statement.close();
+		con.desconectar();
+
+		return rowEliminar;
+	}
+	
+	public boolean actualizar(Categoria nuevo) throws SQLException {
+		boolean rowActualizar = false;
+		String sql = "UPDATE Categoria SET CodigoCategoria=?,NombreCategoria=?,Descripcion=?,Estado=? WHERE CodigoCategoria=?";
+		con.conectar();
+		connection = con.getJdbcConnection();
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString (1,nuevo.getCodigoCategoria());
+		statement.setString(2,nuevo.getNombreCategoria());
+		statement.setString(3,nuevo.getDescripcion());
+		statement.setString(4,nuevo.getEstado());
+		statement.setString(5, nuevo.getCodigoCategoria());
+		System.out.println(nuevo.getCodigoCategoria()+ " holaaaaaaaaaaaaaaaa");
+
+		rowActualizar = statement.executeUpdate() > 0;
+		System.out.println(nuevo.getDescripcion()+ " holaaaaaaaaaaaaaaaa "+ rowActualizar ) ;
+		statement.close();
+		con.desconectar();
+		return rowActualizar;
+	}*/
+	
+	public static void main (String args[]) {
+		Catalogue nuevo = new Catalogue("755","Fiebre","Paracetamol");
+		
+		try {
+			new Cataloguedao().insert(nuevo);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
+
